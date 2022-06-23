0889783da7f784ad5263d9fe7e30a052ddd751d5
Diego <diegoprudencioo@gmail.com>
2022-06-22 21:21:44 -0300

    add hora

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
 //console.log("Helo, world!!!");
+
+//O módulo express retorna uma função que instancia o express
 const express = require("express")
 
+//A função express cria uma instância de todo o framework express em app
 const app = express()
 
-app.listen()
+const porta = 3001
+app.listen(porta, function () {
+    console.log(`Servidor rodando na porta ${porta}`);
+})
+
+app.get("/hora", function (req, resp) {
+   let horaCerta = new Date()
+    resp.send(
+        `
+            <html>
+                <head>
+                    <meta charset="utf-8">
+                </head>
+                <body>
+                    <h1> Hora certa ${horaCerta}</h1>
+                </body>
+            </html>
+        `
+    )
+    
+})
